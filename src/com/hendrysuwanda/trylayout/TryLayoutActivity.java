60259a5af98339b253e6062645b7068745951c22
Hendry Suwanda <hendry_agi@yahoo.com>
2011-10-08 21:11:27 +0700

    Initial Project

---
+++ src/com/hendrysuwanda/trylayout/TryLayoutActivity.java
@@ -0,0 +1,13 @@
+package com.hendrysuwanda.trylayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TryLayoutActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
